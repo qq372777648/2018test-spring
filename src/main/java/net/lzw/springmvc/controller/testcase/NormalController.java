87fa959346f9b9b1c3d8cd16d0d0321858993208
lizhi-lzw <372777648@qq.com>
2018-01-04 10:34:41 +0800

    testcase

---
+++ src/main/java/net/lzw/springmvc/controller/testcase/NormalController.java
@@ -0,0 +1,23 @@
+package net.lzw.springmvc.controller.testcase;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.lzw.springmvc.service.DemoService;
+
+@Controller
+public class NormalController {
+	@Autowired
+	DemoService demoService;
+	
+
+	
+	@RequestMapping("/normal")
+	public  String testPage(Model model){
+		model.addAttribute("msg", demoService.saySomething());
+		return "page";
+	}
+
+}
---
+++ src/test/java/net/lzw/test/spring2/Test.java
@@ -0,0 +1,12 @@
+package net.lzw.test.spring2;
+/** 
+* @author: liangzhenwei
+* @create：2018年1月4日 上午10:26:06 
+* @company:广州荔支网络技术有限公司
+*/
+public class Test {
+	public static Test test(){
+		return new Test();
+	}
+
+}
